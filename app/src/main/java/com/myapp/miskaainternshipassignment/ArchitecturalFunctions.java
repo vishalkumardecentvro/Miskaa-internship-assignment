4117030b85f44c3dc578c52c34beb377290df349
vishal kumar <vishalvlc01@gmail.com>
2021-10-08 23:02:42 +0530

    inital commit

---
+++ app/src/main/java/com/myapp/miskaainternshipassignment/ArchitecturalFunctions.java
@@ -0,0 +1,8 @@
+package com.myapp.miskaainternshipassignment;
+
+public interface ArchitecturalFunctions {
+  void instantiate();
+  void initialize();
+  void listen();
+  void load();
+}
---
+++ app/src/main/java/com/myapp/miskaainternshipassignment/MainActivity.java
@@ -0,0 +1,39 @@
+package com.myapp.miskaainternshipassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements ArchitecturalFunctions {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    instantiate();
+    initialize();
+    listen();
+    load();
+  }
+
+  @Override
+  public void instantiate() {
+
+  }
+
+  @Override
+  public void initialize() {
+
+  }
+
+  @Override
+  public void listen() {
+
+  }
+
+  @Override
+  public void load() {
+
+  }
+}
